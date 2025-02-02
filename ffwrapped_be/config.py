8772dfed19279f2d90010e689bab5a3a0e451715
kevniu95 <kniu@uchicago.edu>
2025-02-01 18:07:54 -0600

    Insert ESPN as platform and define espn extractor and API helper

--- ffwrapped_be/config.py
+++ ffwrapped_be/config.py
@@ -5,15 +5,21 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-class Config:
-    pfref_base = os.getenv('PFREF_BASE')
-    stathead_base = os.getenv('STATHEAD_BASE')
 
-    stathead_username = os.getenv('STATHEAD_USERNAME')
-    stathead_password = os.getenv('STATHEAD_PASSWORD')  
-    
-    railway_db_url = os.getenv('RAILWAY_DB_URL')
-    railway_db_user = os.getenv('RAILWAY_DB_USER')
-    railway_db_password = os.getenv('RAILWAY_DB_PASSWORD')
+class Config:
+    pfref_base = os.getenv("PFREF_BASE")
+    stathead_base = os.getenv("STATHEAD_BASE")
+
+    stathead_username = os.getenv("STATHEAD_USERNAME")
+    stathead_password = os.getenv("STATHEAD_PASSWORD")
+
+    espn_swid = os.getenv("ESPN_SWID")
+    espn_s2 = os.getenv("ESPN_S2")
+    espn_league_id = os.getenv("ESPN_LEAGUE_ID")
+
+    railway_db_url = os.getenv("RAILWAY_DB_URL")
+    railway_db_user = os.getenv("RAILWAY_DB_USER")
+    railway_db_password = os.getenv("RAILWAY_DB_PASSWORD")
+
 
 config = Config()
---
+++ ffwrapped_be/etl/extractors/espn_extractor.py
@@ -0,0 +1,46 @@
+import logging
+from abc import ABC, abstractmethod
+from typing import List, Dict
+
+import espn_api
+from espn_api.football import League
+from ffwrapped_be.config import config
+
+logger = logging.getLogger(__name__)
+
+
+class ESPNExtractor:
+    def __init__(self, league_id: int, year: int, espn_s2: str, swid: str):
+        self.league = League(
+            league_id=league_id,
+            year=year,
+            espn_s2=config.espn_s2,
+            swid=config.espn_swid,
+        )
+
+    def extract_league(self) -> Dict:
+        return self.league
+
+    def extract_teams(self) -> List[Dict]:
+        return self.league.teams
+
+
+if __name__ == "__main__":
+    extractor = ESPNExtractor(
+        config.espn_league_id, 2024, config.espn_s2, config.espn_swid
+    )
+
+    for row in extractor.league.settings.scoring_format:
+        print(row["label"])
+        print(row["points"])
+        print()
+    # print(extractor.league.settings.scoring_format)
+    # teams = extractor.extract_teams()
+    # vs = vars(teams[0])
+    # for k, v in vs.items():
+    #     print(k, v)
+    # vars(team_)
+    # [print(team) for team in extractor.extract_teams()]
+    # league = extractor.league
+
+    # print(league.extract_teams())
