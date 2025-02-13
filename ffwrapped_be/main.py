e2bb0ab25b8d425e8e32131ff41aaea533d91c74
kevniu95 <kniu@uchicago.edu>
2025-02-12 22:02:16 -0600

    Formatting changes

--- ffwrapped_be/main.py
+++ ffwrapped_be/main.py
@@ -8,23 +8,25 @@ load_dotenv()
 # Get the database URL from the environment variable
 DATABASE_URL = os.getenv("RAILWAY_DB_URL")
 
+
 def test_db_connection():
     try:
         # Connect to the PostgreSQL database
         connection = psycopg2.connect(DATABASE_URL)
         cursor = connection.cursor()
-        
+
         # Execute a simple query
         cursor.execute("SELECT version();")
         db_version = cursor.fetchone()
-        
+
         print(f"Connected to the database. PostgreSQL version: {db_version}")
-        
+
         # Close the cursor and connection
         cursor.close()
         connection.close()
     except Exception as error:
         print(f"Error connecting to the database: {error}")
+
 
 if __name__ == "__main__":
     test_db_connection()
